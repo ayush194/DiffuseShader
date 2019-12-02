7511250f1cdaa852766877bf3c87970c4f00cc68
Ayush Kumar <kr.ayush998@gmail.com>
2019-12-02 23:38:11 +0530

    Modified filenames

--- compile.sh
+++ compile.sh
@@ -1,1 +1,11 @@
-g++ -framework OpenGL -lglfw -lglew -I/usr/local/include -I. diffuse_shader_test.cpp objloader.cpp
+#!/bin/sh
+
+if [[ $(uname) =~ "Darwin" ]]
+then
+	g++ -std=c++11 -framework OpenGL -lglfw -lglew -I/usr/local/include -I. -o diffuseshader main.cpp objloader.cpp
+elif [[ $(uname) =~ "Linux" ]]
+then
+	g++ -std=c++11 -I/usr/local/include -I. -o diffuseshader main.cpp objloader.cpp -lglfw -lGLEW -lGL -lGLU
+else
+	echo "This build is not supported for Operating Systems other than Linux and OSX!"
+fi
